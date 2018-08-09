ebe8de26df51aa627cd71d9c9d5985ed739587f4
Michal Kaczkowski <sirmichal23@gazeta.pl>
2018-08-10 00:33:36 +0200

    pierwsza metoda

--- src/nowaKlasa.java
+++ src/nowaKlasa.java
@@ -1,7 +1,17 @@
 public class nowaKlasa {
 public static void main (String [] args){
 // dodanie main
-    
-}
 
+    // wywolanie metody
+
+    System.out.println(dodawanie(2,5));
+
+}
+// metoda
+    public static Integer dodawanie (Integer a, Integer b){
+    int c=a+b;
+
+
+    return c;
+    }
 }
